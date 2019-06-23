ed600d729ff02aa8dfcecf9235eb44d0b904a46c
wangxg <75712197@qq.com>
2019-06-23 23:32:59 +0800

    update

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    'todo-list':TodoList
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ my-vue-project/src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+    props:['content','index'], //定义父组件传过来的属性，必须定义,有多个则定义多个参数 
+    methods:{
+        handleClick(){
+            this.$emit("delete",this.index)
+        }
+    }
+}
+</script>
+
+<style scope>
+
+</style>
